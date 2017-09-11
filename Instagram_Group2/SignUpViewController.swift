94fb5ae5e12eb5d093bf271be5bd56d16997432a
Habib Zarrin Chang Fard <habib@Habibs-MacBook-Pro.local>
2017-09-11 18:07:54 +0800

    done signUp VC

--- Instagram_Group2/SignUpViewController.swift
+++ Instagram_Group2/SignUpViewController.swift
@@ -7,18 +7,73 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
+
 
 class SignUpViewController: UIViewController {
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
-    @IBOutlet weak var createBtnTapped: UIButton!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
     
     
     
+    @IBOutlet weak var createBtnTapped: UIButton!{
     
-
+        didSet{
+        
+         createBtnTapped.addTarget(self, action: #selector(signUp), for: .touchUpInside)
+        }
+    
+    }
+    
+    func signUp() {
+        
+       
+        guard let email = emailTextField.text,
+            let password = passwordTextField.text,
+            let confirmPassword = confirmPasswordTextField.text,
+            let userName = usernameTextField.text
+            
+            else {return}
+        
+        
+        if password != confirmPassword {
+            createErrorVC("Password Error", "Password does not match")
+        } else if email == "" || password == "" || confirmPassword == "" {
+            createErrorVC("Missing input fill", "Please fill up your info appropriately in the respective spaces.")
+        }
+        
+       
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                self.createErrorVC("Error", error.localizedDescription)
+            }
+            
+            if let validUser = user {
+                let ref = Database.database().reference()
+                
+                let post : [String:Any] = ["email": email, "name": userName]
+                
+                ref.child("Users").child(validUser.uid).setValue(post)
+                
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+    }
+    
+    func createErrorVC(_ title: String, _ message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,3 +82,5 @@ class SignUpViewController: UIViewController {
 
    
 }
+
+
