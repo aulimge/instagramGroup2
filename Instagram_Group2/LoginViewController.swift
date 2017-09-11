24e77ca38b74f5e38ae70e5474acf6b764c00c0d
Tan Wei Liang <weiliang98321@gmail.com>
2017-09-11 17:20:44 +0800

    Completed the User Interface for login and sign up page!!

---
+++ Instagram_Group2/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  Instagram_Group2
+//
+//  Created by Tan Wei Liang on 11/09/2017.
+//  Copyright © 2017 Audrey Lim. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginBtnTapped: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    
+
+}
---
+++ Instagram_Group2/SignUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpViewController.swift
+//  Instagram_Group2
+//
+//  Created by Tan Wei Liang on 11/09/2017.
+//  Copyright © 2017 Audrey Lim. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var createBtnTapped: UIButton!
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+   
+}
