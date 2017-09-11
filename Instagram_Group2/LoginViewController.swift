f6b0a2f4af8079271105c0d513ec27aceaa3cf08
Tan Wei Liang <weiliang98321@gmail.com>
2017-09-11 17:46:15 +0800

    updated the code for the login view controller

--- Instagram_Group2/LoginViewController.swift
+++ Instagram_Group2/LoginViewController.swift
@@ -7,18 +7,73 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var loginBtnTapped: UIButton!
+    @IBOutlet weak var loginBtnTapped: UIButton!{
+        didSet{
+            loginBtnTapped.addTarget(self, action: #selector(loginUser), for: .touchUpInside)
+        }
+        
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if Auth.auth().currentUser != nil {
+            guard let vc = storyboard?.instantiateViewController(withIdentifier: "NavigationController") as? UINavigationController else { return }
+            
+            //skip login page straight to homepage
+            present(vc, animated:  true, completion:  nil)
+        }
+        
+
 
        
+    }
+    
+    func loginUser() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            
+            if self.emailTextField.text == "" {
+                self.createErrorAlert("Empty Email Text Field", "Plaese Input Valid Email")
+                return
+            }
+            else if self.passwordTextField.text == "" {
+                self.createErrorAlert("Empty Password Text Field", "Please Input Valid Password")
+                return
+            }
+            if let validError = error {
+                
+                print(validError.localizedDescription)
+                self.createErrorAlert("Error", validError.localizedDescription)
+            }
+            
+            if let validUser = user {
+                guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "NavigationController") as? UINavigationController else { return }
+                
+                self.present(vc, animated:  true, completion:  nil)
+                
+            }
+            
+        }
+        
+    }
+    
+    func createErrorAlert(_ title: String, _ message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Error", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion:  nil)
+        
     }
 
     
