6d70648496b338802b8cb5e0d614861a32ebab70
Tan Wei Liang <weiliang98321@gmail.com>
2017-09-12 17:53:26 +0800

    Add icon for the app and add a photo tab bar controller

---
+++ Instagram_Group2/UploadPhotoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UploadPhotoViewController.swift
+//  Instagram_Group2
+//
+//  Created by Tan Wei Liang on 12/09/2017.
+//  Copyright © 2017 Audrey Lim. All rights reserved.
+//
+
+import UIKit
+
+class UploadPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
