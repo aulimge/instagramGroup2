77a292343dea42d19fa22ddf186dbbd1bb3139e1
Habib Zarrin Chang Fard <habib@Habibs-MacBook-Pro.local>
2017-09-12 11:26:37 +0800

    create edit profile VC and tab bar controller

---
+++ Instagram_Group2/editProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  editProfileViewController.swift
+//  
+//
+//  Created by Habib Zarrin Chang Fard on 12/09/2017.
+//
+//
+
+import UIKit
+
+class editProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+    
+
+}
