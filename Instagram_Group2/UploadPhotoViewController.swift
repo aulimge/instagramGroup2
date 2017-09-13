98d23c2156760be869d9041b69718d288ce84567
Tan Wei Liang <weiliang98321@gmail.com>
2017-09-13 10:19:54 +0800

    upload photo view controller 90% completed and some changes to home view controller

--- Instagram_Group2/UploadPhotoViewController.swift
+++ Instagram_Group2/UploadPhotoViewController.swift
@@ -7,28 +7,126 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseStorage
 
-class UploadPhotoViewController: UIViewController {
-
+class UploadPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     
     @IBOutlet weak var imageView: UIImageView!
     
+    let picker = UIImagePickerController()
+    var profilePicURL : String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        picker.delegate = self
     }
     
-    
-    @IBAction func buttonUpload(_ sender: Any) {
+    @IBAction func buttonUpload(_ sender: UIBarButtonItem) {
+        
+        //photo From Library
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        picker.modalPresentationStyle = .popover
+        present(picker, animated: true, completion: nil)
+        picker.popoverPresentationController?.barButtonItem = sender
         
     }
-
     
-    @IBAction func buttonCamera(_ sender: Any) {
+    @IBAction func buttonCamera(_ sender: UIBarButtonItem) {
+        
+        //shoot camera
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.allowsEditing = false
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            picker.cameraCaptureMode = .photo
+            picker.modalPresentationStyle = .fullScreen
+            present(picker,animated: true,completion: nil)
+        } else {
+            noCamera()
+        }
         
     }
     
+    @IBAction func saveButton(_ sender: AnyObject) {
+        
+        let alert = UIAlertController(title: title, message:"", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Image Uploaded", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion:  nil)
+
+        
+            }
+    
+    
+    func noCamera(){
+        let alertVC = UIAlertController(
+            title: "No Camera",
+            message: "Sorry, this device has no camera",
+            preferredStyle: .alert)
+        let okAction = UIAlertAction(
+            title: "OK",
+            style:.default,
+            handler: nil)
+        alertVC.addAction(okAction)
+        present(
+            alertVC,
+            animated: true,
+            completion: nil)
+    }
+    
+    func uploadToStorage(_ image: UIImage) {
+        
+        let ref = Storage.storage().reference()
+        
+        let timeStamp = Date().timeIntervalSince1970
+        guard let imageData = UIImageJPEGRepresentation(image, 0.5) else { return }
+        
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpeg"
+        
+        ref.child("\(timeStamp).jpeg").putData(imageData, metadata: metaData) { (meta, error) in
+            if let validError = error {
+                print(validError.localizedDescription)
+            }
+            
+            if let validMeta = meta,
+                let downloadPath = meta?.downloadURL()?.absoluteString{
+                self.profilePicURL = downloadPath
+                self.imageView.image = image
+            }
+        }
+    }
+
+    
+    //MARK: - Delegates
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]){
+        var  chosenImage = UIImage()
+        chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+        imageView.contentMode = .scaleAspectFit //3
+        imageView.image = chosenImage //4
+        //dismiss(animated:true, completion: nil) //5
+        
+        
+        defer {
+            dismiss(animated:true, completion: nil)
+        }
+        
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else{
+            return
+        }
+        
+        uploadToStorage(image)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
     
 }
+
