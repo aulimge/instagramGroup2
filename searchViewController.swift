4e4c400bc67eff743b700079d9f89744e669b585
Audrey Lim <audim98@icloud.com>
2017-09-13 10:42:18 +0800

    completed 80% SearchTableView

--- searchViewController.swift
+++ searchViewController.swift
@@ -14,17 +14,24 @@ import FirebaseStorage
 class searchViewController: UIViewController {
 
     var ref : DatabaseReference!
-    var users : [User] = []
+    var contacts : [Contact] = []
     
     @IBOutlet weak var searchTextField: UITextField!
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableView: UITableView! {
+        didSet{
+            tableView.delegate = self
+            tableView.dataSource = self
+
+        }
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        fetchContacts()
     }
     
     
@@ -32,16 +39,75 @@ class searchViewController: UIViewController {
         
     }
     
+    func fetchContacts() {
+        ref = Database.database().reference()
+        
+        //observer child added works as a loop return each child individually
+        ref.child("users").observe(.childAdded, with: { (snapshot) in
+            guard let info =  snapshot.value as? [String : Any] else {return}
+            print("info : \(info)")
+            print(snapshot)
+            print(snapshot.key)
+            
+            //cast snapshot.value to correct Datatype
+            if let username = info["name"] as? String,
+                let fullname = info["fullame"] as? String,
+                let email = info["email"] as? String,
+                let imageURL = info["imageURL"] as? String,
+                let filename = info["filename"] as? String {
+                
+                //create new contact object
+                 let newContact = Contact(anID: snapshot.key, aUsername: username, aFullname: fullname, anEmail: "", anImageURL: imageURL, anFilename: filename)
+                
+                //append to contact array
+                self.contacts.append(newContact)
+                
+                
+                //this is more efficient
+                //insert indv rows as we retrive idv items
+                let  index = self.contacts.count - 1
+                let indexPath = IndexPath(row: index, section: 0)
+                self.tableView.insertRows(at: [indexPath], with: .right)
+                
+                
+            }
+        })
+        
+        ref.child("users").observe(.value, with: {
+            (snapshot) in
+            guard let info = snapshot.value as? [String : Any]
+                else {return}
+            print(info)
+        })
+        
+
+        
+        ref.child("users").observe(.childChanged, with: { (snapshot) in
+            guard let info = snapshot.value as? [String:Any] else {return}
+            
+            guard let username = info["name"] as? String,
+                let fullname = info["fullname"] as? String,
+                let imageURL = info["imageURL"] as? String
+                else {return}
+            
+            if let matchedIndex = self.contacts.index(where: { (contact) -> Bool in
+                return contact.id == snapshot.key
+            }) {
+                let changedContact = self.contacts[matchedIndex]
+                changedContact.username = username
+                changedContact.fullname = fullname
+                changedContact.imageURL = imageURL
+                
+                
+                let indexPath = IndexPath(row: matchedIndex, section: 0)
+                self.tableView.reloadRows(at: [indexPath], with: .none)
+            }
+        })
+        
+        
+    } // fetchContacts
     
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    
-    
 
     /*
     // MARK: - Navigation
@@ -54,3 +120,55 @@ class searchViewController: UIViewController {
     */
 
 }
+
+
+
+extension searchViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as?
+            SearchTableViewCell
+            else { return UITableViewCell() }
+        
+        let contact = contacts[indexPath.row]
+        
+        cell.usernameLabel.text = contact.username
+        cell.fullnameLabel.text = "\(contact.fullname) Following"
+        
+
+        let imageURL = contact.imageURL
+        //cell.profileImageView.image = self.loadImage(from: imageURL)
+        cell.searchImageView.loadImage(from: imageURL)
+        
+        
+        return cell
+        
+    }
+    
+}
+
+
+
+extension searchViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let destination = storyboard?.instantiateViewController(withIdentifier: "ProfileViewController") as? ProfileViewController else {return}
+        
+        
+        let selectedContact = contacts[indexPath.row]
+        
+       // destination.selectedContact = selectedContact
+        navigationController?.pushViewController(destination, animated: true)
+        
+        
+    }
+    
+    
+    
+}
+
+
+
